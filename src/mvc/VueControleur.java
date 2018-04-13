347417b84047abcb32c146c0817e92538ac70a63
Thomas Bourg <tbourg07@gmail.com>
2018-04-13 19:32:25 +0200

    resize on

--- src/mvc/VueControleur.java
+++ src/mvc/VueControleur.java
@@ -16,14 +16,16 @@ import java.util.Observer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
@@ -48,10 +50,46 @@ import javafx.scene.text.Text;
  * @author freder
  */
 public class VueControleur extends Application {
+    
+    Modele m;
 
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
+        BorderPane pane = new BorderPane();
 
+        ComboBox box = new ComboBox();
+        box.setTooltip(new Tooltip("Quelle taille désirez-vous?"));
+
+        box.getItems().add(3);
+        box.getItems().add(4);
+        box.getItems().add(5);
+        box.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                try {
+                    Stage game = new Stage();
+                    game.setTitle("Partie de " + box.getValue() + "x" + box.getValue());
+                    game.setScene(buildGame((int) box.getValue()));
+                    game.show();
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(VueControleur.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        pane.setCenter(box);
+
+        pane.setLeft(new Text("Sélectionnez la taille de la grille:"));
+
+        Scene scene = new Scene(pane, Color.LIGHTBLUE);
+
+        primaryStage.setTitle("Labyrinthe");
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(300);
+        primaryStage.show();
+    }
+
+    public Scene buildGame(int size) throws FileNotFoundException {
         // gestion du placement (permet de palcer le champ Text affichage en haut, et GridPane gPane au centre)
         BorderPane border = new BorderPane();
 
@@ -59,133 +97,132 @@ public class VueControleur extends Application {
         affichage.setFont(Font.font("Verdana", 15));
         affichage.setFill(Color.BLACK);
         border.setTop(affichage);
-        
-        TabPane tabs =new TabPane();
 
-        for (int i = 3; i < 6; i++) {
-            final int size = i;
+        // permet de placer les diffrents boutons dans une grille
+        GridPane gPane = new GridPane();
 
-            // permet de placer les diffrents boutons dans une grille
-            GridPane gPane = new GridPane();
-
-            Label[][] tab = new Label[size][size];
-            // initialisation du modèle que l'on souhaite utiliser
-            Modele m = new Modele(size, 0, 0, 1, 2, 2, 0, 2, 2);
-
-            // la vue observe les "update" du modèle, et réalise les mises à jour graphiques
-            m.addObserver(new Observer() {
-
-                @Override
-                public void update(Observable o, Object arg) {
-                    // TODO
-                    if (!m.grille.win) {
-                        List<Node> list = new ArrayList<>(gPane.getChildren());
-                        gPane.getChildren().clear();
-                        for (int column = 0; column < size; column++) {
-                            for (int row = 0; row < size; row++) {
-                                try {
-                                    Label l = (Label) list.get(size * column + row);
-                                    Case c = m.grille.tab[column][row];
-                                    ImageView img;
-                                    switch (c.etat) {
-                                        case 0:
-                                            l.setGraphic(null);
-                                            break;
-                                        case 1:
-                                            img = new ImageView(new Image(new FileInputStream(c.symbole.getPath())));
-                                            img.setFitHeight(100);
-                                            img.setFitWidth(100);
-                                            l.setGraphic(img);
-                                            break;
-                                        case 2:
-
-                                            img = new ImageView(new Image(new FileInputStream(c.lien.getPath())));
-                                            img.setFitHeight(100);
-                                            img.setFitWidth(100);
-                                            l.setGraphic(img);
-                                            break;
-                                    }
-                                    gPane.add(l, column, row);
-                                } catch (FileNotFoundException ex) {
-                                    Logger.getLogger(VueControleur.class.getName()).log(Level.SEVERE, null, ex);
-                                }
-                            }
-                        }
-                    } else {
-                        gPane.getChildren().clear();
-                        gPane.add(new Text("VICTOIRE !!!"), 0, 0, size, size);
-                        gPane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-                    }
-                }
-            });
-
-            for (int column = 0; column < size; column++) {
-                for (int row = 0; row < size; row++) {
-
-                    final int fColumn = column;
-                    final int fRow = row;
-
-                    final Label l = new Label("");
-                    l.setMinHeight(100);
-                    l.setMinWidth(100);
-                    if (m.grille.tab[column][row].estSymbole()) {
-                        ImageView img = new ImageView(new Image(new FileInputStream(m.grille.tab[column][row].symbole.getPath())));
-                        img.setFitHeight(100);
-                        img.setFitWidth(100);
-                        l.setGraphic(img);
-                    }
-                    l.setBackground(new Background(new BackgroundFill(Color.rgb(223, 135, 134), CornerRadii.EMPTY, Insets.EMPTY)));
-                    tab[column][row] = l;
-                    l.setFont(Font.font("Verdana", 25));
-
-                    l.setOnDragDetected(new EventHandler<MouseEvent>() {
-                        public void handle(MouseEvent event) {
-
-                            Dragboard db = l.startDragAndDrop(TransferMode.ANY);
-                            ClipboardContent content = new ClipboardContent();
-                            content.putString(""); // non utilisé actuellement
-                            db.setContent(content);
-                            event.consume();
-                            m.startDD(fColumn, fRow);
-                        }
-                    });
-
-                    l.setOnDragEntered(new EventHandler<DragEvent>() {
-                        public void handle(DragEvent event) {
-
-                            m.parcoursDD(fColumn, fRow);
-                            event.consume();
-                        }
-                    });
-
-                    l.setOnDragDone(new EventHandler<DragEvent>() {
-                        public void handle(DragEvent event) {
-
-                            // attention, le setOnDragDone est déclenché par la source du Drag&Drop
-                            m.stopDD(fColumn, fRow);
-
-                        }
-                    });
-
-                    gPane.add(tab[column][row], column, row);
-                }
-            }
-
-            gPane.setHgap(5);
-            gPane.setVgap(5);
-            gPane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-            
-            Tab t = new Tab("tab de size "+size, gPane);
-            tabs.getTabs().add(t);
+        Label[][] tab = new Label[size][size];
+        // initialisation du modèle que l'on souhaite utiliser
+        switch (size) {
+            case 3:
+                m = new Modele(size, 0, 0, 1, 2, 2, 0, 2, 2);
+                break;
+            case 4:
+                m = new Modele(size, 0, 0, 3, 0, 1, 0, 2, 2);
+                break;
+            case 5:
+                m = new Modele(size, 0, 0, 1, 1, 1, 2, 3, 3);
+                break;
         }
 
-        border.setCenter(tabs);
+        // la vue observe les "update" du modèle, et réalise les mises à jour graphiques
+        m.addObserver(new Observer() {
+
+            @Override
+            public void update(Observable o, Object arg) {
+                // TODO
+                if (!m.grille.win) {
+                    List<Node> list = new ArrayList<>(gPane.getChildren());
+                    gPane.getChildren().clear();
+                    for (int column = 0; column < size; column++) {
+                        for (int row = 0; row < size; row++) {
+                            try {
+                                Label l = (Label) list.get(size * column + row);
+                                Case c = m.grille.tab[column][row];
+                                ImageView img;
+                                switch (c.etat) {
+                                    case 0:
+                                        l.setGraphic(null);
+                                        break;
+                                    case 1:
+                                        img = new ImageView(new Image(new FileInputStream(c.symbole.getPath())));
+                                        img.setFitHeight(100);
+                                        img.setFitWidth(100);
+                                        l.setGraphic(img);
+                                        break;
+                                    case 2:
+
+                                        img = new ImageView(new Image(new FileInputStream(c.lien.getPath())));
+                                        img.setFitHeight(100);
+                                        img.setFitWidth(100);
+                                        l.setGraphic(img);
+                                        break;
+                                }
+                                gPane.add(l, column, row);
+                            } catch (FileNotFoundException ex) {
+                                Logger.getLogger(VueControleur.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                    }
+                } else {
+                    gPane.getChildren().clear();
+                    gPane.add(new Text("VICTOIRE !!!"), 0, 0, size, size);
+                    gPane.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+                }
+            }
+        });
+
+        for (int column = 0; column < size; column++) {
+            for (int row = 0; row < size; row++) {
+
+                final int fColumn = column;
+                final int fRow = row;
+
+                final Label l = new Label("");
+                l.setMinHeight(100);
+                l.setMinWidth(100);
+                if (m.grille.tab[column][row].estSymbole()) {
+                    ImageView img = new ImageView(new Image(new FileInputStream(m.grille.tab[column][row].symbole.getPath())));
+                    img.setFitHeight(100);
+                    img.setFitWidth(100);
+                    l.setGraphic(img);
+                }
+                l.setBackground(new Background(new BackgroundFill(Color.rgb(223, 135, 134), CornerRadii.EMPTY, Insets.EMPTY)));
+                tab[column][row] = l;
+                l.setFont(Font.font("Verdana", 25));
+
+                l.setOnDragDetected(new EventHandler<MouseEvent>() {
+                    public void handle(MouseEvent event) {
+
+                        Dragboard db = l.startDragAndDrop(TransferMode.ANY);
+                        ClipboardContent content = new ClipboardContent();
+                        content.putString(""); // non utilisé actuellement
+                        db.setContent(content);
+                        event.consume();
+                        m.startDD(fColumn, fRow);
+                    }
+                });
+
+                l.setOnDragEntered(new EventHandler<DragEvent>() {
+                    public void handle(DragEvent event) {
+
+                        m.parcoursDD(fColumn, fRow);
+                        event.consume();
+                    }
+                });
+
+                l.setOnDragDone(new EventHandler<DragEvent>() {
+                    public void handle(DragEvent event) {
+
+                        // attention, le setOnDragDone est déclenché par la source du Drag&Drop
+                        m.stopDD(fColumn, fRow);
+
+                    }
+                });
+
+                gPane.add(tab[column][row], column, row);
+            }
+        }
+
+        gPane.setHgap(5);
+        gPane.setVgap(5);
+        gPane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        border.setCenter(gPane);
 
         Scene scene = new Scene(border, Color.LIGHTBLUE);
 
-        primaryStage.setTitle("Labyrinthe");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        return scene;
     }
 
     /**
